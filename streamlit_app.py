f11ce3ff9d3342b83be9158fc4087d37dfebc9d1
AstroTaffy <tiffanycnash@gmail.com>
2025-02-06 08:13:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 # session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 #Convert the Snowpark data frame to pandas to use the loc function
 pd_df = my_dataframe.to_pandas()
